aba23cbfabe6d211c83a65d1dfe71b05d17f787c
James Woolfenden <james.woolfenden@gmail.com>
2022-10-11 15:10:33 +0100

    nodegroup and role

---
+++ aws_eks_node_group.pike.tf
@@ -0,0 +1,24 @@
+resource "aws_eks_node_group" "pike" {
+  cluster_name    = aws_eks_cluster.pike.name
+  node_group_name = "pike"
+  node_role_arn   = aws_iam_role.nodegroup.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
